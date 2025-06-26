7f1fd6d67ba7096009e1b4304b904f8c05dd826c
TIASherine <sherine24ti@mahasiswa.pcr.ac.id>
2025-06-26 08:47:38 +0700

    lagi

---
+++ konekDatabase.php
@@ -0,0 +1,14 @@
+<?php
+$hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "topupstore";
+
+    $koneksi = new mysqli($hostname, $username, $password, $database);
+
+    if($koneksi->connect_error) {
+        die("Koneksi Gagal");
+    } else {
+        echo "Koneksi Berhasil";
+    }
+?>
