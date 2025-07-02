98bc862c6d279821606f39dd7edf10cd229c3d86
TIASherine <sherine24ti@mahasiswa.pcr.ac.id>
2025-07-02 23:17:42 +0700

    tambah sedikit

---
+++ index.php
@@ -0,0 +1,8 @@
+<body>
+    <input type="text" name="name" id="name" placeholder="Name" required>
+    <input type="password" name="pass" id="pass" placeholder="Password" required>
+</body>
+
+<?php
+
+?>
