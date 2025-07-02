c5fc1d0552e1d9f01683a141c30cd79c0f0f0138
TIASherine <sherine24ti@mahasiswa.pcr.ac.id>
2025-07-02 23:26:33 +0700

    tambah sedikit

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <body>
-    <input type="text" name="name" id="name" placeholder="Name" required>
-    <input type="password" name="pass" id="pass" placeholder="Password" required>
+    <form action="home.html" method="post">
+        <div class="center">
+            <div class="contact">
+                <input type="text" name="name" id="name" placeholder="Name" required> <br>
+                <input type="password" name="pass" id="pass" placeholder="Password" required>
+
+                <br> <br>
+
+                <input type="submit" value="Login">
+            </div>
+        </div>
+    </form>
 </body>
 
 <?php
